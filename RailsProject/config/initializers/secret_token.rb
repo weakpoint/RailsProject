664c1260bd9313101d2a1189d1b4ebf49b94cc6a
weak_point <emil.1990@interia.pl>
2013-12-19 14:25:47 +0100

    Initialize repository

---
+++ RailsProject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsProject::Application.config.secret_token = '28827f4ce86759c706ad0ae4316140c017ffa67741ada2cdfb6ada36d794087f11ef68b0f4f26b362b4c24221ee8379a6a2b2a4e478d26b2c16860445be650ae'
