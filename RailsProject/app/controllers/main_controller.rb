19e16d366e01d190898aa50b34e8772f482f5f83
weak_point <emil.1990@interia.pl>
2013-12-23 18:16:26 +0100

    Main view part 2

--- RailsProject/app/controllers/main_controller.rb
+++ RailsProject/app/controllers/main_controller.rb
@@ -1,8 +1,19 @@
 class MainController < ApplicationController
 	
+	
   def index
-  @dayImageURL = "http://pogodynka.pl/http/assets/products/main_page_maps/"
-  @linkToDayImage = @dayImageURL+"day_2013-12-23d.jpg"
-  @linkToNightImage = @dayImageURL+"day_2013-12-23n.jpg"
+  generateListOfDays()
+  @dayImageURL = "http://pogodynka.pl/http/assets/products/main_page_maps/day_"
+  generateLinks()
+  end
+	
+  def generateListOfDays
+  require 'date'
+  @days = {"Teraz" => "0000-00-00", "Niedziela" =>Date.today.to_s}
+  end
+  
+  def generateLinks
+  @linkToDayImage = @dayImageURL+"0000-00-00d.jpg"
+  @linkToNightImage = @dayImageURL+"0000-00-00n.jpg"
   end
 end
