6d0d3a4357b7f3271b108be52a0c0ba2a2904240
weak_point <emil.1990@interia.pl>
2013-12-24 15:02:55 +0100

    Main View finished

--- RailsProject/app/controllers/main_controller.rb
+++ RailsProject/app/controllers/main_controller.rb
@@ -1,19 +1,20 @@
 class MainController < ApplicationController
-	
-	
+		
   def index
   generateListOfDays()
   @dayImageURL = "http://pogodynka.pl/http/assets/products/main_page_maps/day_"
-  generateLinks()
+  @linkToDayImage = @dayImageURL+"0000-00-00d.jpg"
+  @linkToNightImage = @dayImageURL+"0000-00-00n.jpg"
   end
 	
   def generateListOfDays
   require 'date'
-  @days = {"Teraz" => "0000-00-00", "Niedziela" =>Date.today.to_s}
-  end
+  @days = Hash.new
+  @days["Teraz"] = "0000-00-00"
+  @days["Dzisiaj"] = Date.today.to_s
   
-  def generateLinks
-  @linkToDayImage = @dayImageURL+"0000-00-00d.jpg"
-  @linkToNightImage = @dayImageURL+"0000-00-00n.jpg"
+  for i in 1..5
+   @days[(Date.today+i).to_s] = (Date.today+i).to_s
+	end
   end
 end
