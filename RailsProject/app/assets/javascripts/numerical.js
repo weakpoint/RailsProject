3b016599e3d87ce557bb852e1c4479816828c928
weak_point <emil.1990@interia.pl>
2013-12-27 11:57:46 +0100

    numerical date and time hotfix

--- RailsProject/app/assets/javascripts/numerical.js
+++ RailsProject/app/assets/javascripts/numerical.js
@@ -1,8 +1,9 @@
 var mapImage = document.getElementById("mapImage");
 var citySelect = document.getElementById("citySelect");
+var dateandtime = document.getElementById("dateandtime");
 
 citySelect.onchange = function(){
 
-  mapImage.src = "http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=2013122406&"+this.options[this.selectedIndex].value+"&lang=pl";
+  mapImage.src = "http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate="+dateandtime.innerHTML+"&"+this.options[this.selectedIndex].value+"&lang=pl";
   
 }
